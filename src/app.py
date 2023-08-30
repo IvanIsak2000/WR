577e2ffde9e1b78b3c957ccbf1e2bd1997825ed5
IvanIsak2000 <vingving648@gmail.com>
2023-08-30 22:54:45 +0300

    fix `post` func, add fake user data

--- src/app.py
+++ src/app.py
@@ -1,22 +1,30 @@
 from flask import Flask
 from flask import render_template, request
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
-import requests
+
+
+fake_db: list[dict] = []
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home_page():
     return render_template('home.html')
+
 
 @app.route('/explore')
 def explore():
     return render_template('explore.html')
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login',methods = ['POST'])
 def login():
-    username = request.form.get('username') 
-    print(username)
-    # your code
-    # return a response
+    fake_name = 'iwan'
+    user = request.form['nm']
+    return 'Valid name' if user == fake_name else 'Not valid'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
