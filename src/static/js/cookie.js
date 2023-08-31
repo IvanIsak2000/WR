9ea4d9961f62b168812e2edd284c19481c5a3b7e
IvanIsak2000 <vingving648@gmail.com>
2023-08-31 14:22:39 +0300

    fix `x` position to `y`

--- src/static/js/cookie.js
+++ src/static/js/cookie.js
@@ -1,6 +1,6 @@
 const tl = gsap.timeline({defaults:{duration: 0.5, ease: "power3.out"}})
 
-tl.fromTo('.cookie-container', {opacity: 0, x: -200}, {opacity: 1, x:0, duration:2})
+tl.fromTo('.cookie-container', {opacity: 0, y: -200}, {opacity: 1, y:0, duration:2})
 
 const cookie_accept = document.querySelector('.accept');
 
